eb85d6d6cf8458196b358d6ef962a0671dadcd19
trangqt2012 <trang.quach@staff.gumiviet.com>
2021-04-18 17:45:02 +0700

    feat (contact): add contact

--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    protected $table='contact';
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'content',
+        'id_user'
+    ];
 }
